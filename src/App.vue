f090e068cc791da0d0a3abdbd5487b420dc75c3e
Kevin From <kevinfrom@live.dk>
2020-05-04 17:44:39 +0200

    Finished birthday app just because

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="{background: bgColor }">
+    <div class="box" v-if="bdayToday">
+      <h1><span class="first">Happy</span> <span class="second">birthday</span> <span class="third">Thomas!</span></h1>
+    </div>
+    <div class="box" v-else>It ain't Thomas's birthday today...</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      bgColor: 'white',
+      bdayToday: false
+    }
+  },
+  mounted() {
+    const today = new Date();
+    this.bdayToday = today.getDate() === 5 && today.getMonth() === 4;
+
+    if (this.bdayToday) {
+      this.$confetti.start();
+
+      this.bgColor = 'rgb(68, 173, 157)';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap');
+
+* {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+}
+
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.first {
+  color: #C32C48;
+}
+
+.second {
+  color: #75B3A7;
+}
+
+.third {
+  color: #EFBF6A;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueConfetti from 'vue-confetti'
 
 Vue.config.productionTip = false
+Vue.use(VueConfetti)
 
 new Vue({
   render: h => h(App),
